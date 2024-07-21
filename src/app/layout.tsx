69fbd85e46a57874bfae5048a74d5f2af9b12526
cobmin <jacob-huber@outlook.com>
2024-07-20 19:36:01 -0500

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,55 +1,55 @@
-import type { Metadata } from 'next';
-import { Inter as FontSans } from 'next/font/google';
-import '@/styles/globals.css';
-import { cn } from '@/lib/utils';
-import MyNavigationMenu from '@/components/my-navigation-menu';
-import { metadata as siteMetadata } from '@/lib/metadata';
-import { ThemeProvider } from '@/components/theme-provider';
-import MyFooter from '@/components/my-footer-menu';
-import { cookieToInitialState } from 'wagmi'
+  import type { Metadata } from 'next';
+  import { Inter as FontSans } from 'next/font/google';
+  import '@/styles/globals.css';
+  import { cn } from '@/lib/utils';
+  import MyNavigationMenu from '@/components/my-navigation-menu';
+  import { metadata as siteMetadata } from '@/lib/metadata';
+  import { ThemeProvider } from '@/components/theme-provider';
+  import MyFooter from '@/components/my-footer-menu';
+  import { cookieToInitialState } from 'wagmi'
 
-import { config } from '@/config'
-import Web3ModalProvider from '@/context'
-import { headers } from 'next/headers';
+  import { config } from '@/config'
+  import Web3ModalProvider from '@/context'
+  import { headers } from 'next/headers';
 
-const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
+  const fontSans = FontSans({
+    subsets: ['latin'],
+    variable: '--font-sans',
+  });
 
-export const metadata: Metadata = {
-  ...siteMetadata,
-  title: {
-    default: "Cobmin's Blog",
-    template: '%s | Cobmin',
-  },
-};
+  export const metadata: Metadata = {
+    ...siteMetadata,
+    title: {
+      default: "Cobmin's Blog",
+      template: '%s | Cobmin',
+    },
+  };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const initialState = cookieToInitialState(config, headers().get('cookie'))
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased flex flex-col',
-          fontSans.variable
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+  export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const initialState = cookieToInitialState(config, headers().get('cookie'))
+    return (
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            'min-h-screen bg-background font-sans antialiased flex flex-col',
+            fontSans.variable
+          )}
         >
-          <Web3ModalProvider initialState={initialState}>
-          <MyNavigationMenu />
-          <div className="flex-grow flex justify-center p-2">
-            <div className="w-full max-w-4xl">{children}</div>
-          </div>
-          <MyFooter />
-        </Web3ModalProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Web3ModalProvider initialState={initialState}>
+            <MyNavigationMenu />
+            <div className="flex-grow flex justify-center p-2">
+              <div className="w-full max-w-4xl">{children}</div>
+            </div>
+            <MyFooter />
+          </Web3ModalProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    );
+  }
--- src/context/index.tsx
+++ src/context/index.tsx
@@ -14,13 +14,17 @@ const queryClient = new QueryClient()
 
 if (!projectId) throw new Error('Project ID is not defined')
 
-// Create modal
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
+    createWeb3Modal({
+        wagmiConfig: config,
+        projectId,
+        enableAnalytics: true, // Optional - defaults to your Cloud configuration
+        enableOnramp: true, // Optional - false as default
+        themeVariables: {
+            '--w3m-color-mix': '#D1C4E9', // Subtle purple color
+            '--w3m-color-mix-strength': 1, // Adjust strength as needed
+            '--w3m-accent': '#6A0DAD', // Accent color for buttons, icons, labels, etc.
+        },
+});
 
 export default function Web3ModalProvider({
   children,
